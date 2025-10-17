d82c1fa105bc927c5e68c4f9d91e635391850b11
choi <cjy1749@naver.com>
2025-10-17 17:08:50 +0900

    10-17 commit choi

--- shoppy-fullstack-app/backend/src/main/java/com/springboot/shoppy_fullstack_app/controller/MemberController.java
+++ shoppy-fullstack-app/backend/src/main/java/com/springboot/shoppy_fullstack_app/controller/MemberController.java
@@ -1,35 +1,40 @@
 package com.springboot.shoppy_fullstack_app.controller;
 
 import com.springboot.shoppy_fullstack_app.dto.Member;
+import com.springboot.shoppy_fullstack_app.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/member")
 @CrossOrigin(origins = {"http://localhost:3000"})
 public class MemberController {
+    //서비스 객체 가져오기
+    private final MemberService memberService;
+
+    @Autowired
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
     @PostMapping("/login")
     public boolean login(@RequestBody Member member) {
-        boolean result = false;
-        System.out.println(member.getId());
-        System.out.println(member.getPwd());
-        if(member.getId().equals("test") && member.getPwd().equals("1234")) result = true;
-
+        boolean result = memberService.login(member);
         return result;
     }
     @PostMapping("/signup")
     public boolean signup (@RequestBody Member member) {
-        boolean result = true;
-        System.out.println(member.getId());
-        System.out.println(member.getPwd());
-        System.out.println(member.getName());
-        System.out.println(member.getPhone());
-        System.out.println(member.getEmail());
+        boolean result = false;
+        //서비스 호출
+        int rows = memberService.signup(member);
+        if(rows == 1) result = true;
 
         return result;
     }
     @PostMapping("/idcheck")
     public String idcheck (@RequestBody Member member) {
-        boolean result = false;     //아이디 존재:1, 존재x:0
+        boolean result = memberService.idCheck(member.getId());
+
         String msg = "";
         if(result) msg = "이미 사용중인 아이디 입니다.";
         else msg = "사용이 가능한 아이디 입니다.";
---
+++ shoppy-fullstack-app/backend/src/main/java/com/springboot/shoppy_fullstack_app/repository/JdbcTemplateMemberRepository.java
@@ -0,0 +1,46 @@
+package com.springboot.shoppy_fullstack_app.repository;
+
+import com.springboot.shoppy_fullstack_app.dto.Member;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import javax.sql.DataSource;
+
+@Repository
+public class JdbcTemplateMemberRepository implements MemberRepository{
+    private final JdbcTemplate jdbcTemplate;
+
+    public JdbcTemplateMemberRepository (DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);   //커넥션생성
+    }
+
+
+
+    @Override
+    public int save (Member member) {
+        //jdbcTemplate객체를 이용하여 DB의 member테이블에 insert
+        String sql = "insert into member(id, pwd, name, phone, email, mdate) values(?, ?, ?, ?, ?, now())";
+        Object[] param = {
+            member.getId(), member.getPwd(), member.getName(), member.getPhone(), member.getEmail()
+        };
+
+        int rows = jdbcTemplate.update(sql, param);
+        System.out.println("rows => " + rows);
+        return rows;
+    }
+
+    @Override
+    public Long findId (String id) {
+        String sql = "select count(id) from member where id=?";
+        Long count = jdbcTemplate.queryForObject(sql, Long.class, id);
+        return count;
+    }
+
+    @Override
+    public Long checkId (Member member) {
+        String sql = "select count(id) from member where id=? and pwd=?";
+        Long count = jdbcTemplate.queryForObject(sql, Long.class, member.getId(), member.getPwd());
+        return count;
+    }
+}
